3d52f59dac0348e417b4b6da61189963a8e21f94
Samuel-Cavicchi <samcavicchi@gmail.com>
2018-12-09 17:00:55 +0100

    Minor clean up changes

--- app/wallet-debts/wallet-debts.js
+++
@@ -1,8 +0,0 @@
-db = require('../database.js');
-const routes = require("express").Router();
-
-routes.get("/:id", function (req, res) {
-    res.status(200).json("Found wallet debts / id")
-})
-
-module.exports = routes;
