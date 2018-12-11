b1adeabac7be3ac0f13d0b6aa18bfef64e3d9eec
josefwakman <josef.wakman@gmail.com>
2018-12-11 15:00:21 +0100

    laid groundwork

--- app/aws-sdk.js
+++ app/aws-sdk.js
@@ -1,3 +1,4 @@
+
 const AWS = require('aws-sdk')
 
 
@@ -6,9 +7,53 @@ const credentials = new AWS.Credentials(
     "4bJQHCZlvuFTrHpLUEgmv9HR7zQJZE0UOMST4nCQ"
 )
 
-const s3 = new AWS.s3 ({
+const sts = new AWS.STS ({
     credentials: credentials,
-    region: "s3.eu-central-1.amazonaws.com" //endpoint. Maybe it should be something else?
+    region: "eu-central-1" // Maybe this should be something else?
 })
 
-s3.putOb
+const policy = `{ "Version": "2012-10-17",
+    "Statement": [{
+        "Action": [
+            "s3:PutObject"
+        ],
+    "Effect": "Allow",
+    "Resource": "arn:aws:s3:::wallet-fox-images/*"
+    }]
+}`
+
+sts.assumeRole({
+    RoleArn: "arn:aws:iam::373772666655:user/rainmaker",
+    RoleSessionName: "rainmaker-upload",
+    DurationSeconds: 15 * 60, // 15 minutes
+    Policy: policy
+}, function(error, data) {
+    /* Send the following to client:
+    data.Credentials.AccessKeyId
+    data.Credentials.SecretAccessKey
+    data.Credentials.SessionToken */
+
+    const body = {
+        accessKeyId: data.Credentials.AccessKeyId,
+        secretAccessKey: data.Credentials.SectretAccessKey,
+        sessionToken: data.Credentials.SessionToken
+    }
+
+    return body
+})
+
+
+const fileToUploadName = "placeholder"
+const fileToUpLoadBody = "också placeholder"
+
+s3.putObject({
+    Bucket: "arn:aws:s3:::wallet-fox-images",
+    Key: fileToUpload,        
+    Body: fileToUpLoadBody 
+}, function(error, data) {
+    if(error){
+        print(":(")
+    } else {
+        print(":)")
+    }
+})
