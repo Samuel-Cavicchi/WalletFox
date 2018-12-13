d33bb1ddf4d0a7aa8bc7f8e8bfff4e93c87645d4
josefwakman <josef.wakman@gmail.com>
2018-12-13 11:22:18 +0100

    not working :(

--- app/aws-sdk.js
+++ app/aws-sdk.js
@@ -22,38 +22,47 @@ const policy = `{ "Version": "2012-10-17",
     }]
 }`
 
-sts.assumeRole({
-    RoleArn: "arn:aws:iam::373772666655:user/rainmaker",
-    RoleSessionName: "rainmaker-upload",
-    DurationSeconds: 15 * 60, // 15 minutes
-    Policy: policy
-}, function(error, data) {
-    /* Send the following to client:
-    data.Credentials.AccessKeyId
-    data.Credentials.SecretAccessKey
-    data.Credentials.SessionToken */
+function assumeRole() {
+    console.log("Assuming role")
+    var giveMeTheData = ""
+    return new Promise (function(resolve, reject) {
+        sts.assumeRole({
+            RoleArn: "arn:aws:iam::373772666655:role/justtesting",
+            RoleSessionName: "rainmaker-upload",
+            DurationSeconds: 15 * 60, // 15 minutes
+            Policy: policy
+        }, function(error, data) {
+            /* Send the following to client:
+            data.Credentials.AccessKeyId
+            data.Credentials.SecretAccessKey
+            data.Credentials.SessionToken */
+            console.log("Error: ", error)
+            console.log("Data: ", data)
+            
+            if (error) {
+                reject(error)
+            } else {
+                resolve(data)
+            }
 
-    const body = {
-        accessKeyId: data.Credentials.AccessKeyId,
-        secretAccessKey: data.Credentials.SectretAccessKey,
-        sessionToken: data.Credentials.SessionToken
-    }
-
-    return body
-})
+        })  
+    })
+} 
 
 
-const fileToUploadName = "placeholder"
-const fileToUpLoadBody = "också placeholder"
+// const fileToUploadName = "placeholder"
+// const fileToUpLoadBody = "också placeholder"
 
-s3.putObject({
-    Bucket: "arn:aws:s3:::wallet-fox-images",
-    Key: fileToUpload,        
-    Body: fileToUpLoadBody 
-}, function(error, data) {
-    if(error){
-        print(":(")
-    } else {
-        print(":)")
-    }
-})
+// s3.putObject({
+//     Bucket: "arn:aws:s3:::wallet-fox-images",
+//     Key: fileToUpload,        
+//     Body: fileToUpLoadBody 
+// }, function(error, data) {
+//     if(error){
+//         print(":(")
+//     } else {
+//         print(":)")
+//     }
+// })
+
+module.exports.assumeRole = assumeRole
--- app/users/users.js
+++ app/users/users.js
@@ -1,9 +1,17 @@
 db = require('../database.js');
 auth = require('../auth.js');
 reqhandler = require('../request-handler.js');
+const aws = require('../aws-sdk.js')
 const routes = require("express").Router();
 
+
 routes.patch("/:id", function (req, res) {
+    const testing = aws.assumeRole().then(
+        testing => 
+        console.log("Testing: ", testing)
+    ).catch(console.log("fuck"))
+
+
     const body = req.body
     const id = req.params.id
     const missingParameters = reqhandler.checkRequestParams({ request: req, requiredBody: ['token'], })
@@ -18,11 +26,7 @@ routes.patch("/:id", function (req, res) {
             db.getUser(id).then(user => {
                 for (key in user) {
                     if (body[key] != undefined) { // In the user object all values that matches a key of the body are replaced
-                        if (key == "image")  {
-                            
-                        } else {
-                            user[key] = body[key]
-                        }
+                        user[key] = body[key]
                     }
                 }
                 db.updateUser(id, user).then(() => {
