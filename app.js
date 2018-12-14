a6de3c492d49f228dbf5ad950dae4d145e39db8d
Samuel-Cavicchi <samcavicchi@gmail.com>
2018-12-14 17:09:20 +0100

    removed changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const routes = require("./app/routes.js");
 const db = require("./app/database.js");
 const bodyParser = require('body-parser') // TODO: Move this to a more fitting place after discussion
 // var port = process.env.PORT || 8080
+
 const app = express();
 app.set('trust proxy', true)
 app.use(bodyParser.urlencoded({ extended: false })) // Set bodyparser to JSON? Need to look this up
@@ -10,10 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false })) // Set bodyparser to JSON? N
 // Re-route all paths to our routes.js file
 app.use("/", routes);
 
-const host = server.address().address;
-const port = server.address().port;
 
-console.log(`Example app listening at http://${host}:${port}`);
 app.listen(port, () => {
     console.log('Wallet Fox listening on port:', port); // DELETE ON SUBMISSION
 })
