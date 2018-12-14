1b257b248ed7eae0d6c4e60383ad020e659f9e0a
Samuel-Cavicchi <samcavicchi@gmail.com>
2018-12-14 17:03:12 +0100

    yaml change

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const routes = require("./app/routes.js");
 const db = require("./app/database.js");
 const bodyParser = require('body-parser') // TODO: Move this to a more fitting place after discussion
-var port = process.env.PORT || 8080
-
+// var port = process.env.PORT || 8080
+var port = process.env.PORT
 const app = express();
+app.set('trust proxy', true)
 app.use(bodyParser.urlencoded({ extended: false })) // Set bodyparser to JSON? Need to look this up
 
 // Re-route all paths to our routes.js file
